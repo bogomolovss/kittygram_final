c719d5480283788ecd1b20b31dd1b3bf2f5091f1
Faburlas <44576572+bogomolovss@users.noreply.github.com>
2023-06-23 19:49:24 +0300

    fix errors

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.67.120', '127.0.0.1', 'bogomolovss.ru']
+ALLOWED_HOSTS = ['51.250.110.87', '127.0.0.1', 'bogomolovss.ru']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
